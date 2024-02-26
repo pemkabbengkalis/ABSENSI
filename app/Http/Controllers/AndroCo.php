2f7c424765db1267ffe7df28ba4022724bbcc139
pendekarcoding <bohatimulyadi99@gmail.com>
2024-02-26 07:24:48 +0700

    update handle absen

--- app/Http/Controllers/AndroCo.php
+++ app/Http/Controllers/AndroCo.php
@@ -296,10 +296,12 @@ public function login(Request $r){
     $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Masuk')->first();
     $checkabsen = AbsenModel::where('id_pegawai',$data->id_pegawai)->where('kode_unitkerja',$data->kode_unitkerja)->where('tglabsen',date('Y-m-d'))->where('jenis','M')->count();
     $checkabsen = ($jam == null) ? "no":"yes";
+    $checkabsen = ($checkabsen > 0) ? "no":"yes";
   }else if($dayTerm=='Afternoon'){
     $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Pulang')->first();
     $checkabsen = AbsenModel::where('id_pegawai',$data->id_pegawai)->where('kode_unitkerja',$data->kode_unitkerja)->where('tglabsen',date('Y-m-d'))->where('jenis','P')->count();
     $checkabsen = ($jam == null) ? "no":"yes";
+    $checkabsen = ($checkabsen > 0) ? "no":"yes";
     }else{
       $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Pulang')->first();
       $checkabsen = "no";
@@ -916,10 +918,12 @@ public function getdatabyId(Request $r){
     $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Masuk')->first();
     $checkabsen = AbsenModel::where('id_pegawai',$r->id)->where('kode_unitkerja',$data->kode_unitkerja)->where('tglabsen',date('Y-m-d'))->where('jenis','M')->count();
     $checkabsen = ($jam == null) ? "no":"yes";
+    $checkabsen = ($checkabsen > 0) ? "no":"yes";
   }else if($dayTerm=='Afternoon'){
     $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Pulang')->first();
     $checkabsen = AbsenModel::where('id_pegawai',$r->id)->where('kode_unitkerja',$data->kode_unitkerja)->where('tglabsen',date('Y-m-d'))->where('jenis','P')->count();
     $checkabsen = ($jam == null) ? "no":"yes";
+    $checkabsen = ($checkabsen > 0) ? "no":"yes";
     }else{
       $jam = JamModel::where('hari',$hari)->where('kode_unitkerja',$data->kode_unitkerja)->where('jenis','Jam Pulang')->first();
       $checkabsen = "no";
