e90ab3a1738849236a8cdef48ea8bf358dd99e65
pendekarcoding <bohatimulyadi99@gmail.com>
2024-10-15 10:52:44 +0700

    add method post

--- resources/views/theme/login/login.blade.php
+++ resources/views/theme/login/login.blade.php
@@ -24,7 +24,7 @@ $apps = AplikasiModel::where('id_app','1')->first();
       <div class="logo">
       </div>
       <div class="login-box">
-        <form class="login-form" action="{{url('loginpost')}}"> {{csrf_field()}}
+        <form class="login-form" action="{{url('loginpost')}}" method="post"> {{csrf_field()}}
         <center>
           <img src="{{url('logobks.png')}}" alt="Girl in a jacket" width="70" height="90" >
             @include('theme.Layouts.alert')
